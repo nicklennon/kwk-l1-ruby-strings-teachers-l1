839c97db7f22dc29085c3804ad5282f0ab81c766
nicklennon <nicklennon.53@gmail.com>
2018-06-17 15:16:30 +0000

    Done.

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,28 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
+
+# User inputs and store guest_name.
+# puts "To whom should the invitation be addressed? Ex. Hermione"
+# Disagree with this but required for learn. 
+puts "Hi, you've been invited to a party! What is your name?"
+guest_name = gets.strip
+
+# User inputs and store party_name
+# disagree with this but required for learn. 
+puts "What is the name of your party? Ex. the Best Halloween Party Ever"
+party_name = gets.strip
+
+# User inputs and store date
+puts "What date is the party? Ex. October 31"
+date = gets.strip.capitalize
+
+# User inputs and store time
+puts "What time will the festivities commence? Ex. 6pm"
+time = gets.strip
+
+# User inputs and store host_name
+puts "Who is hosting? Ex. Harry Potter"
+host_name = gets.strip
+
+puts "Dear #{guest_name},\n\nYou are cordially invited to #{party_name} on #{date} at #{time}. Please send your regrets only.\n\nSincerely,\n\n#{host_name}"
