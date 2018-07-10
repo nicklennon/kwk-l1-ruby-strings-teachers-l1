55ad13371b92deeb78865dd8dde06a11845797bf
nicklennon <nicklennon.53@gmail.com>
2018-07-10 10:21:54 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -2,10 +2,15 @@
 
 # Try starting out with puts'ing a string.
 
+<<<<<<< HEAD
 # User inputs and store guest_name.
 # puts "To whom should the invitation be addressed? Ex. Hermione"
 # Disagree with this but required for learn. 
 puts "Hi, you've been invited to a party! What is your name?"
+=======
+# User inputs and store guest_name
+puts "To whom should the invitation be addressed? Ex. Hermione"
+>>>>>>> 2d5a5d78e315d87cb31302c3de468e53f2c1e21a
 guest_name = gets.strip
 
 # User inputs and store party_name
